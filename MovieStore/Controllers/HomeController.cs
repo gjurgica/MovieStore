fcb71f4d0eb71b9ce4f65825a3b161e7d7b7f57e
Gjurgica Anastasovska <gurgica78@gmail.com>
2019-06-10 18:57:32 +0200

    first commit

---
+++ MovieStore/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MovieStore.Models;
+
+namespace MovieStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static List<MovieModel> Movies = new List<MovieModel>
+        {
+
+                new MovieModel(1, "Fantastic Mr. Fox", "Wes Anderson", "An urbane fox cannot resist returning to his farm raiding ways and then must help his community survive the farmers' retaliation.", 265),
+                new MovieModel(2, "The Life Aquatic with Steve Zissou", "Wes Anderson", "With a plan to exact revenge on a mythical shark that killed his partner, Oceanographer Steve Zissou rallies a crew that includes his estranged wife, a journalist, and a man who may or may not be his son.", 250),
+                new MovieModel(3, "Rushmore", "Wes Anderson", "The extracurricular king of Rushmore Preparatory School is put on academic probation.", 350)
+
+        };
+        public IActionResult Index()
+        {
+
+            return View(Movies);
+        }
+
+        public IActionResult About()
+        {
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+
+            return View();
+        }
+        public IActionResult Edit(int id)
+        {
+            var movie = Movies.FirstOrDefault(m => m.Id == id);
+
+            return View(movie);
+        }
+        public IActionResult Add()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SaveChanges(MovieModel movie)
+        {
+            if (!ModelState.IsValid) {
+                return View("Edit");
+            }
+            var index = Movies.FindIndex(i => i.Id == movie.Id);
+            Movies[index] = movie;
+            return View("Index", Movies);
+        }
+        [HttpPost]
+        public IActionResult Add(MovieModel movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            Movies.Add(movie);
+            return View("Index", Movies);
+        }
+        public IActionResult Delete(int id)
+        {
+            var movie = Movies.FirstOrDefault(m => m.Id == id);
+            Movies.Remove(movie);
+            return View("Index",Movies);
+        }
+    }
+}
---
+++ MovieStore/Models/MovieModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieStore.Models
+{
+    public class MovieModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public MovieModel() { }
+        public MovieModel(int id,string title,string director,string description,decimal price)
+        {
+            Id = id;
+            Title = title;
+            Director = director;
+            Description = description;
+            Price = price;
+        }
+    }
+}
